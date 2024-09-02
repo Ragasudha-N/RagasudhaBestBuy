430cfae20d203843b8697b146eee94d8fe4298e6
ragasu <ragasu@LIN-HN0Q6Q3.corp.capgemini.com>
2024-09-02 14:53:25 +0530

    First commit

---
+++ src/test/java/stepDefinition/Menu.java
@@ -0,0 +1,17 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.ParameterType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.*;
+
+public class Menu {
+    
+    }
+
+
